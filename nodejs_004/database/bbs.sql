eb005773e6f23157fa9ff26ec9e4d488e768fe34
hooninfinity <hooninfinity@naver.com>
2021-08-23 16:42:27 +0900

    ejs

---
+++ nodejs_004/database/bbs.sql
@@ -0,0 +1,12 @@
+-- 모든 서버에서 접속할 수 있게 하겠다
+-- % : 어디에서나 접근가능
+CREATE USER 'node'@'%' identified by '12341234';
+
+-- 모든 권한 부여
+GRANT ALL privileges ON *.* TO 'node'@'%';
+
+CREATE DATABASE nodeDB;
+USE nodeDB;
+DESC tbl_bbs;
+DROP TABLE tbl_bbs;
+DESC tbl_bbs;
