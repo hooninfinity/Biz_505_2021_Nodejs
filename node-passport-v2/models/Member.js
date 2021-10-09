c4fa6e357dba8ddbbc28b7f747827ce412c9142d
hooninfinity <hooninfinity@naver.com>
2021-10-09 14:50:15 +0900

    passport-v2

---
+++ node-passport-v2/models/Member.js
@@ -0,0 +1,36 @@
+const admin = {
+  userid: "admin",
+  password: "12345",
+  role: "ADMIN",
+  email: "test@korea.com",
+};
+
+const user = {
+  userid: "user",
+  password: "12345",
+  role: "user",
+  email: "user@daum.com",
+};
+
+const guest = {
+  userid: "guest",
+  password: "12345",
+  role: "guest",
+  email: "guest@google.com",
+};
+
+const hoon = {
+  userid: "hoon",
+  password: "12345",
+  role: "user",
+  email: "hoon@naver.com",
+};
+
+const hong = {
+  userid: "hong",
+  password: "12345",
+  role: "guest",
+  email: "hong@gwang.com",
+};
+
+export const members = [admin, user, guest, hoon, hong];
